36fb1b77a09e15141cf536985564f64928d33c1d
lisaanthro <liza_vater@yahoo.com>
2024-03-09 19:44:54 +0300

    big fixes

--- algorithms/data_matching_functions.py
+++ algorithms/data_matching_functions.py
@@ -3,36 +3,36 @@ import random
 
 
 def create_project_role_ids(data: Dict[str, List[Dict[str, Any]]]) -> Dict[str, List[str]]:
-    """For each project_role create a list of tasks_ids with this project_role"""
+    """For each project_role create a list of resourse_ids with this project_role"""
 
-    tasks_by_project_roles: Dict[str, List[str]] = {}
-    for task in data.get("tasks"):
-        project_role = task.get("project_role_id")
-        if project_role not in tasks_by_project_roles:
-            tasks_by_project_roles[project_role] = [task.get("id")]
+    resources_by_project_roles: Dict[str, List[str]] = {}
+    for resource in data.get("resources"):
+        project_role = resource.get("project_role_id")
+        if project_role not in resources_by_project_roles:
+            resources_by_project_roles[project_role] = [resource.get("id")]
         else:
-            tasks_by_project_roles[project_role].append(task.get("id"))
+            resources_by_project_roles[project_role].append(resource.get("id"))
 
-    return tasks_by_project_roles
+    return resources_by_project_roles
 
 
 def match_task_id_to_resourse_id(data: Dict[str, List[Dict[str, Any]]],
-                                 tasks_by_project_roles: Dict[str, List[str]]) -> Dict[str, str]:
+                                 resources_by_project_roles: Dict[str, List[str]]) -> Dict[str, str]:
     """Randomly assign resourse_id to each task_id based on project_role"""
 
-    for project_role_id, project_role_id_list in tasks_by_project_roles.items():
+    for project_role_id, project_role_id_list in resources_by_project_roles.items():
         random.shuffle(project_role_id_list)
 
-    last_ids = dict(zip(tasks_by_project_roles.keys(), [0] * len(tasks_by_project_roles)))
+    last_ids = dict(zip(resources_by_project_roles.keys(), [0] * len(resources_by_project_roles)))
     tasks_to_resources = {}
 
     for task in data.get("tasks"):
         project_role_id = task.get("project_role_id")
         last_id = last_ids.get(project_role_id)
-        tasks_to_resources[task["id"]] = tasks_by_project_roles[project_role_id][last_id]
-        if last_id == len(tasks_by_project_roles.get(project_role_id)):
-            random.shuffle(tasks_by_project_roles[project_role_id])
+        tasks_to_resources[task.get("id")] = resources_by_project_roles[project_role_id][last_id]
+
+        if last_id == len(resources_by_project_roles.get(project_role_id)):
+            random.shuffle(resources_by_project_roles[project_role_id])
             last_ids["project_role_id"] = 0
 
     return tasks_to_resources
-
--- algorithms/graph_dependencies/topological_sort.py
+++ algorithms/graph_dependencies/topological_sort.py
@@ -2,14 +2,14 @@ from graph import create_graph, get_roots
 
 
 class TopSort:
-    def __init__(self, data):
+    def __init__(self, data: dict):
         self.graph = create_graph(data.get("dependencies"))
         self.roots = get_roots(self.graph, data.get("dependencies"))
         self.tin = {}
         self.tout = {}
         self.time = 0
 
-    def dfs(self, vertex, par):
+    def dfs(self, vertex: str, par: str) -> None:
         self.tin[vertex] = self.time
         self.time += 1
         for to in self.graph.get(vertex, []):
@@ -19,9 +19,9 @@ class TopSort:
         self.tout[vertex] = self.time
         self.time += 1
 
-    def top_sort(self):
+    def top_sort(self) -> None:
         for root in self.roots:
-            self.dfs(root, -1)
+            self.dfs(root, "-1")
 
-    def is_parent(self, upper_vertex, lower_vertex):
+    def is_parent(self, upper_vertex: str, lower_vertex: str) -> bool:
         return self.tin[upper_vertex] <= self.tin[lower_vertex] and self.tout[upper_vertex] >= self.tout[lower_vertex]
