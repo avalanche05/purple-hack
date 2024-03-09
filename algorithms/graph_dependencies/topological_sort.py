c11c8959c4858cd1d0f0d4733c4da43b80ff3e4a
lisaanthro <liza_vater@yahoo.com>
2024-03-09 20:29:43 +0300

    simulated annealing modified

--- algorithms/graph_dependencies/topological_sort.py
+++ algorithms/graph_dependencies/topological_sort.py
@@ -5,6 +5,7 @@ class TopSort:
     def __init__(self, data: dict):
         self.graph = create_graph(data.get("dependencies"))
         self.roots = get_roots(self.graph, data.get("dependencies"))
+        self.task_sequence = []
         self.tin = {}
         self.tout = {}
         self.time = 0
@@ -12,6 +13,8 @@ class TopSort:
     def dfs(self, vertex: str, par: str) -> None:
         self.tin[vertex] = self.time
         self.time += 1
+        self.task_sequence.append(vertex)
+
         for to in self.graph.get(vertex, []):
             if to != par:
                 self.dfs(to, vertex)
@@ -23,5 +26,12 @@ class TopSort:
         for root in self.roots:
             self.dfs(root, "-1")
 
+    def gen_sequence(self) -> list:
+        return self.task_sequence
+
     def is_parent(self, upper_vertex: str, lower_vertex: str) -> bool:
         return self.tin[upper_vertex] <= self.tin[lower_vertex] and self.tout[upper_vertex] >= self.tout[lower_vertex]
+
+    def can_swap(self, upper_vertex: str, lower_vertex: str) -> bool:
+        return not self.is_parent(upper_vertex, lower_vertex)
+
--- algorithms/simulated_annealing.py
+++ algorithms/simulated_annealing.py
@@ -20,27 +20,50 @@ class SimulatedAnnealing:
     def fit(self, *args) -> None:
         pass
 
-    def predict(self, data):
-        tasks_by_project_roles = create_project_role_ids(data)
-        tasks_to_resourses = match_task_id_to_resourse_id(data, tasks_by_project_roles)
-        # ans = combined_cost_fn(...)
+    def predict_sequence(self, task_ids, tasks_to_resources):
+        task_sequence = task_ids.copy()
+        random.shuffle(task_sequence)
+        ans = assign_time(task_ids, tasks_to_resources)
+
+        for i in range(self.num_iterations):
+            self.temp *= self.temp_decay
+            first_id = random.randint(0, len(task_ids))
+            second_id = random.randint(0, len(task_ids))
+
+            if can_swap(task_ids[first_id], task_ids[second_id]):
+                annealed_task_sequence = task_sequence.copy()
+                annealed_task_sequence[first_id], annealed_task_sequence[second_id] = annealed_task_sequence[second_id], \
+                annealed_task_sequence[first_id]
+                annealed_ans = assign_time(annealed_task_sequence, tasks_to_resources)
+                if annealed_ans < ans or random.uniform(0, 1) < math.exp((annealed_ans - ans) / self.temp):
+                    task_sequence = annealed_task_sequence
+                    ans = annealed_ans
+
+        return ans
+
+    def predict_resource(self, data):
+        resources_py_project_roles = create_project_role_ids(data)
+        tasks_to_resources = match_task_id_to_resourse_id(data, resources_py_project_roles)
+        project_roles = list(resources_py_project_roles.keys())
         ans = self.cost_fn(...)
 
         for i in range(self.num_iterations):
             self.temp *= self.temp_decay
-            annealed_tasks_to_resourses = tasks_to_resourses.copy()
-            project_role_id = random.randint(0, len(tasks_by_project_roles))
-            first_id = tasks_by_project_roles[project_role_id][
-                random.randint(0, len(tasks_by_project_roles[project_role_id]))]  # task id
-            second_id = tasks_by_project_roles[project_role_id][
-                random.randint(0, len(tasks_by_project_roles[project_role_id]))]
-            annealed_tasks_to_resourses[first_id], annealed_tasks_to_resourses[second_id] = \
-                annealed_tasks_to_resourses[second_id], annealed_tasks_to_resourses[first_id]
+            annealed_tasks_to_resources = tasks_to_resources.copy()
+            project_role_id = random.choice(project_roles)
+            first_id = resources_py_project_roles[project_role_id][
+                random.randint(0, len(resources_py_project_roles[project_role_id]))]  # task id
+            second_id = resources_py_project_roles[project_role_id][
+                random.randint(0, len(resources_py_project_roles[project_role_id]))]
+            annealed_tasks_to_resources[first_id], annealed_tasks_to_resources[second_id] = \
+                annealed_tasks_to_resources[second_id], annealed_tasks_to_resources[first_id]
 
-            # annealed_ans = combined_cost_fn(...)
-            annealed_ans = self.cost_fn(...)
+            # annealed_ans = self.cost_fn(...)
+            sequence_simulated_annealing = SimulatedAnnealing(1.0, 1000)
+            annealed_ans = sequence_simulated_annealing.predict_sequence(...)
+
             if annealed_ans < ans or random.uniform(0, 1) < math.exp((annealed_ans - ans) / self.temp):
-                tasks_to_resourses = annealed_tasks_to_resourses
+                tasks_to_resources = annealed_tasks_to_resources
                 ans = annealed_ans
 
 
