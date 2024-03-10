3d8fb576a3a1c8cb09883a2e8dc7843b9b7242f2
Ilin Ivan <buran05@icloud.com>
2024-03-10 19:35:24 +0300

    change annealing iteration count

--- backend/app/utils/calculate.py
+++ backend/app/utils/calculate.py
@@ -4,23 +4,26 @@ from algorithms.simulated_annealing import SimulatedAnnealing
 from algorithms.graph_dependencies import topological_sort
 from algorithms import cost_functions
 
+
 def process_json(data: dict, duration: float, price: float, resource: float) -> dict:
     init(data)
     topological_sort.init()
     cost_functions.init()
 
+    print(duration, price, resource)
     if duration == 1 and price == 0 and resource == 0:
-        simulated_annealing = SimulatedAnnealing(cost_len_fn, 1.0, 0.99, 1000)
+        simulated_annealing = SimulatedAnnealing(cost_len_fn, 1.0, 0.99, 100)
         ans, assigned_time_list = simulated_annealing.predict_resource(data_lists)
     elif duration == 0 and price == 1 and resource == 0:
-        simulated_annealing = SimulatedAnnealing(cost_len_fn, 1.0, 0.99, 1000)
+        simulated_annealing = SimulatedAnnealing(cost_len_fn, 1.0, 0.99, 100)
         ans, assigned_time_list = simulated_annealing.predict_resource(data_lists, "price")
     elif duration == 0 and price == 0 and resource == 1:
-        simulated_annealing = SimulatedAnnealing(cost_len_fn, 1.0, 0.99, 1000)
+        print("INN")
+        simulated_annealing = SimulatedAnnealing(cost_len_fn, 1.0, 0.99, 100)
         ans, assigned_time_list = simulated_annealing.predict_resource(data_lists, "resource")
     else:
         simulated_annealing = SimulatedAnnealing(
-            combined_cost_fn(cost_len_fn, cost_fn, cost_resource_fn, duration, price, resource), 1.0, 0.99, 1000)
+            combined_cost_fn(cost_len_fn, cost_fn, cost_resource_fn, duration, price, resource), 1.0, 0.99, 100)
         ans, assigned_time_list = simulated_annealing.predict_resource(data_lists)
 
     return get_output(assigned_time_list)
